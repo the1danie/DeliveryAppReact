d027e1c2d11e863c409c23d6b4ab5480a501d042
Danie <36858305+the1danie@users.noreply.github.com>
2024-07-18 11:49:45 +0000

    70% edit graphs

--- src/components/Statistics/Main/SubCategory/TopGraph.jsx
+++ src/components/Statistics/Main/SubCategory/TopGraph.jsx
@@ -1,84 +1,37 @@
-import "./TopGraph.css";
 import React from "react";
-import { Line } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-} from "chart.js";
+import MyResponsiveLine from "./MyResponsiveLine"; // Adjust the path as per your file structure
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement);
-
-const TopGraph = () => {
-  const data = {
-    labels: [
-      "January",
-      "",
-      "February",
-      "",
-      "March",
-      "",
-      "April",
-      "",
-      "May",
-      "",
-      "June",
-      "",
-      "July",
-      "",
-      "August",
-      "",
-      "September",
-      "",
-      "October",
-      "",
-      "November",
-      "",
-      "December",
+const data = [
+  {
+    id: "First dataset",
+    data: [
+      { x: "Jan", y: 33 },
+      { x: "Feb", y: 53 },
+      { x: "Mar", y: 85 },
+      { x: "Apr", y: 41 },
+      { x: "May", y: 44 },
+      { x: "Jun", y: 65 },
     ],
-    datasets: [
-      {
-        label: "Total Users",
-        backgroundColor: "transparent",
-        borderColor: "rgba(75,192,192,1)",
-        pointBorderColor: "transparent",
-        pointBorderWidth: 4,
-        pointRadius: 5, // Hide points
-        data: [65, 59, 80, 81, 56, 55, 40, 70, 90, 100, 85, 95],
-        tension: 0.5,
-      },
+  },
+  {
+    id: "Second dataset",
+    data: [
+      { x: "Jan", y: 33 },
+      { x: "Feb", y: 25 },
+      { x: "Mar", y: 35 },
+      { x: "Apr", y: 51 },
+      { x: "May", y: 54 },
+      { x: "Jun", y: 76 },
     ],
-  };
+  },
+];
 
-  const options = {
-    scales: {
-      x: {
-        grid: {
-          display: false,
-        },
-      },
-      y: {
-        min: 20,
-        max: 100,
-        ticks: {
-          stepSize: 20,
-          callback: (value) => value + "K",
-        },
-      },
-    },
-    grid: {
-      borderDash: [10],
-    },
-  };
+const App = () => (
+  <div style={{ height: "400px" }}>
+    {" "}
+    {/* Ensure a defined height */}
+    <MyResponsiveLine data={data} />
+  </div>
+);
 
-  return (
-    <div className="top-graph-container">
-      <h2>Statistics Graph</h2>
-      <Line data={data} options={options} />
-    </div>
-  );
-};
-
-export default TopGraph;
+export default App;
