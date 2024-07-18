840bda051b84d610cd9c62e3f308ed1bc5eb73f3
Danie <36858305+the1danie@users.noreply.github.com>
2024-07-18 09:32:41 +0000

    some changes graphs

--- src/components/Statistics/Main/SubCategory/TopGraph.jsx
+++ src/components/Statistics/Main/SubCategory/TopGraph.jsx
@@ -1,53 +1,82 @@
 import "./TopGraph.css";
 import React from "react";
-import { Bar } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
+  PointElement,
+  LineElement,
 } from "chart.js";
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement);
 
 const TopGraph = () => {
   const data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: [
+      "January",
+      "",
+      "February",
+      "",
+      "March",
+      "",
+      "April",
+      "",
+      "May",
+      "",
+      "June",
+      "",
+      "July",
+      "",
+      "August",
+      "",
+      "September",
+      "",
+      "October",
+      "",
+      "November",
+      "",
+      "December",
+    ],
     datasets: [
       {
-        label: "User Statistics",
-        backgroundColor: "rgba(75,192,192,0.4)",
+        label: "Total Users",
+        backgroundColor: "transparent",
         borderColor: "rgba(75,192,192,1)",
-        borderWidth: 1,
-        hoverBackgroundColor: "rgba(75,192,192,0.6)",
-        hoverBorderColor: "rgba(75,192,192,1)",
-        data: [65, 59, 80, 81, 56, 55, 40],
+        pointBorderColor: "transparent",
+        pointBorderWidth: 4,
+        pointRadius: 5, // Hide points
+        data: [65, 59, 80, 81, 56, 55, 40, 70, 90, 100, 85, 95],
+        tension: 0.5,
       },
     ],
   };
 
   const options = {
     scales: {
-      y: {
-        beginAtZero: true,
+      x: {
+        grid: {
+          display: false,
+        },
       },
+      y: {
+        min: 20,
+        max: 100,
+        ticks: {
+          stepSize: 20,
+          callback: (value) => value + "K",
+        },
+      },
+    },
+    grid: {
+      borderDash: [10],
     },
   };
 
   return (
     <div className="top-graph-container">
       <h2>Statistics Graph</h2>
-      <Bar data={data} options={options} />
+      <Line data={data} options={options} />
     </div>
   );
 };
