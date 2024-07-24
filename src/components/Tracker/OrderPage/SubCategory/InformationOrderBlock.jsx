29730fb0726823a44313e062d2317ef15159cd84
danie667 <bombuuk@gmail.com>
2024-07-24 10:33:47 +0500

    also need to add comments and done tracker page

--- src/components/Tracker/OrderPage/SubCategory/InformationOrderBlock.jsx
+++ src/components/Tracker/OrderPage/SubCategory/InformationOrderBlock.jsx
@@ -3,6 +3,8 @@ import './InformationOrderBlock.css';
 import GreyVector from '../../../../assets/VectorgreyVector.svg';
 import PurpleVector from '../../../../assets/VectorpurpleVector.svg';
 import Line from '../../../../assets/Vector 2189Line.svg'
+import Man from '../../../../assets/man.png';
+import Woman from '../../../../assets/Avatar.png';
 
 const InformationOrderBlock = () => {
     return (
@@ -19,8 +21,8 @@ const InformationOrderBlock = () => {
                             Москва, Малая Юшуньская улица, 1к1
                         </p>
                     </div>
-                    <div style={{position: 'absolute', marginLeft: '280px', marginBottom: '95px'}}>
-                        <img src={Line} alt="" height={'190px'}/>
+                    <div style={{position: 'absolute', marginLeft: '280px', marginBottom: '100px'}}>
+                        <img src={Line} alt="" height={'240px'}/>
                     </div>
                     <div className="order-section">
                         <div style={{display: 'flex', gap: '10px', alignItems: 'flex-start'}}>
@@ -50,35 +52,52 @@ const InformationOrderBlock = () => {
             <div className="details-container">
                 <div className="details-card">
                     <div className="details-card-header">
-                        <img src={Line} alt="Отправитель"/>
+                        <img src={Man} alt="Отправитель"/>
                         <span>Отправитель</span>
                     </div>
-                    <div className="details-field">
-                        <span className="details-field-label">Имя</span>
-                        <span className="details-field-value">Иван</span>
-                    </div>
-                    <div className="details-field">
-                        <span className="details-field-label">Фамилия</span>
-                        <span className="details-field-value">Иванов</span>
-                    </div>
-                    <div className="details-field">
-                        <span className="details-field-label">Отчество</span>
-                        <span className="details-field-value">Иванович</span>
-                    </div>
-                    <div className="details-field">
-                        <span className="details-field-label">ИНН</span>
-                        <span className="details-field-value">123456789123</span>
-                    </div>
-                    <div className="details-field">
-                        <span className="details-field-label">Номер телефона</span>
-                        <span className="details-field-value">+1 (777) 458 - 78 - 94</span>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '20px',
+                        border: '1px solid #ddd',
+                        borderRadius: '8px',
+                        padding: '20px'
+                    }}>
+                        <div className="details-field">
+                            <span className="details-field-label">Имя</span>
+                            <span className="details-field-value">Иван</span>
+                        </div>
+                        <div className="details-field">
+                            <span className="details-field-label">Фамилия</span>
+                            <span className="details-field-value">Иванов</span>
+                        </div>
+                        <div className="details-field">
+                            <span className="details-field-label">Отчество</span>
+                            <span className="details-field-value">Иванович</span>
+                        </div>
+                        <div className="details-field">
+                            <span className="details-field-label">ИНН</span>
+                            <span className="details-field-value">123456789123</span>
+                        </div>
+                        <div className="details-field details-field-custom">
+                            <span className="details-field-label">Номер телефона</span>
+                            <span className="details-field-value">+1 (777) 458 - 78 - 94</span>
+                        </div>
                     </div>
                 </div>
                 <div className="details-card">
                     <div className="details-card-header">
-                        <img src={Line} alt="Отправитель"/>
+                        <img src={Woman} alt="Отправитель"/>
                         <span>Курьер</span>
                     </div>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '20px',
+                        border: '1px solid #ddd',
+                        borderRadius: '8px',
+                        padding: '20px'
+                    }}>
                     <div className="details-field">
                         <span className="details-field-label">Имя</span>
                         <span className="details-field-value">Ирина</span>
@@ -95,9 +114,10 @@ const InformationOrderBlock = () => {
                         <span className="details-field-label">ИНН</span>
                         <span className="details-field-value">123456789123</span>
                     </div>
-                    <div className="details-field">
+                    <div className="details-field details-field-custom">
                         <span className="details-field-label">Номер телефона</span>
                         <span className="details-field-value">+1 (777) 458 - 78 - 94</span>
+                    </div>
                     </div>
                 </div>
             </div>
